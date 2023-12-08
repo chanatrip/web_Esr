7c7715633f2d8d96e924bdcb6d446f1ea17f3a6c
bird <chanatriprtaf@hotmail.com>
2023-12-08 22:47:51 +0700

    save file not use

---
+++ html/notuse/index2 copy.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+        table {
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        th, td {
+            border: 1px solid black;
+            padding: 8px;
+            text-align: center;
+            font-size: 18px;
+            white-space: normal; /* ให้ข้อมูลยาวขึ้นบรรทัดใหม่เมื่อเกินขนาด */
+        }
+
+        th {
+            border-bottom: 1px solid black;
+        }
+
+        td.no-wrap {
+            white-space: nowrap; /* ป้องกันข้อมูลขึ้นบรรทัดใหม่ในคอลัม์ที่ไม่ต้องการ */
+        }
+
+        body {
+            margin: 0px;
+        }
+    </style>
+</head>
+<body>
+
+<table>
+    <tr>
+        <th colspan="9">แบบรายงานสถานภาพ ใบแบบ รด.สอ.๐๕</th>
+    </tr>
+    <tr>
+        <td colspan="1">นชย.</td>
+        <td colspan="2">แบบ คมส.</td>
+        <td colspan="3">หมายเลข</td>
+        <td colspan="2">ESR.No.</td>
+        <td colspan="1">W/O No.</td>
+    </tr>
+    <tr>
+        <td colspan="1">COM.</td>
+        <td colspan="2">ACCS</td>
+        <td colspan="3">ALL</td>
+        <td colspan="2">ESR.No.</td>
+        <td colspan="1">W/O No.</td>
+    </tr>
+    <tr>
+        <td colspan="2">คู่แจ้ง</td>
+        <td colspan="3">ว.ด.ป.รายงาน</td>
+        <td colspan="2">ประมาณระยะเวลาประมาณระยะเวลา</td>
+        <td colspan="2">รหัสสถานภาพ</td>
+    </tr>
+    <tr>
+        <td colspan="2">คู่แจ้ง</td>
+        <td colspan="3">ว.ด.ป.รายงาน</td>
+        <td colspan="2">ประมาณระยะเวลา</td>
+        <td colspan="2">รหัสสถานภาพ</td>
+    </tr>
+    <tr>
+        <td colspan="5" >เหตุขัดข้อง / เนื่องจาก</td>
+        <td colspan="4">ปิดสถานภาพ</td>
+    </tr>
+    <tr>
+        <td colspan="5" >หกดฟกำหแอผหกแอผหกแผหกแผหกแ</td>
+        <td colspan="4">วัน / เดือน / ปี  เวลา  คู่แจ้ง</td>
+    </tr>
+    <tr style="height: 1000px;">
+        <th colspan="9"></th>
+    </tr>
+</table>
+<!-- <table>
+    <tr>
+        <th colspan="9">ใบสั่งงาน ใบแบบ ส.ทอ.๐๕</th>
+    </tr>
+    <tr>
+        <td colspan="1">หน่วยซ่อมย่อย</td>
+        <td colspan="3">แบบ คมส.</td>
+        <td colspan="3">หมายเลขเครื่อง</td>
+        <td colspan="2">หมายเลขใบสั่งงาน</td>
+    </tr>
+    <tr>
+        <td colspan="1">com</td>
+        <td colspan="3">ICSS</td>
+        <td colspan="3">ALL</td>
+        <td colspan="2">3/10</td>
+    </tr>
+    <tr>
+        <td colspan="1">ESR.No.</td>
+        <td colspan="2">ว.ด.ป.เปิด</td>
+        <td colspan="2">ว.ด.ป.ปิด</td>
+        <td colspan="1">ชม./คน</td>
+        <td colspan="3">ค่าแรง (บาท)</td>
+
+    </tr>
+    <tr>
+        <td colspan="5">อาการขัดข้อง</td>
+        <td colspan="4">AWM วัน</td>
+    </tr>
+    <tr>
+        <td colspan="5">การแก้ไข / การดำเนินงาน</td>
+        <td colspan="4">AWM วัน</td>
+    </tr>
+    <tr>
+        <td colspan="5">ชิ้นส่วนที่ใช้</td>
+        <td colspan="4">ยผส. วัน ค่าชิ้นส่วน</td>
+
+    </tr>
+    <tr>
+        <td style="text-align: left;" colspan="3">
+        ลงชื่อผู้ปฏิบัติงาน (เวรผลัด B ) <br> 
+        พ.อ.อ.อดิศักดิ์ กลิญสังข์<br>
+        พ.อ.อ.อดิศักดิ์ กลิญสังข์<br>
+        พ.อ.อ.อดิศักดิ์ กลิญสังข์<br>
+        พ.อ.อ.อดิศักดิ์ กลิญสังข์</td>
+        <td colspan="2" style="text-align: left;">ลงชื่อ <br>จนท.คลัง<br>ลงชื่อ</td>
+        <td colspan="4" style="text-align: left;">ลงชื่อ คคภ.ผู้ตรวจ <br>วันที่ <br>ลงชื่อ หน.ซบร.</td>
+    </tr>
+</table> -->
+
+</body>
+</html>
